4633b983b78cc706fd610911d502803943c0a4e5
Takuya Kitazawa <k.takuti@gmail.com>
2018-01-16 07:37:19 +0900

    Refactor

--- scripts/recommend.py
+++ scripts/recommend.py
@@ -7,6 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 RE_FRONT_MATTER = re.compile('---\n([\s\S]*?\n)---\n')
+RE_PATH_TO_PERMALINK = re.compile('(/note/.+?)(\.md|\.html)')
 
 tagger = MeCab.Tagger('-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
 tagger.parse('')
@@ -23,7 +24,11 @@ def tokenizer(doc):
     return [token for token in get_tokens(doc)]
 
 
-def compute_similarities(paths):
+def path_to_permalink(path):
+    return RE_PATH_TO_PERMALINK.search(path).group(1) + '/'
+
+
+def recommend_content_based_cf(paths, k=3):
     samples = []
     for path in paths:
         with open(path) as f:
@@ -35,9 +40,15 @@ def compute_similarities(paths):
         content = content.replace(m.group(0), '')
         samples.append(content)
 
+    # build model
     vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, tokenizer=tokenizer)
     tfidf = vectorizer.fit_transform(samples)
-    return cosine_similarity(tfidf)
+    similarities = cosine_similarity(tfidf)
+
+    for i, path in enumerate(paths):
+        # find top-k most-similar articles (except for target article itself which is similarity=1.0)
+        top_indices = np.argsort(similarities[i, :])[::-1][1:(k + 1)]
+        yield (path, [path_to_permalink(paths[j]) for j in top_indices])
 
 
 def process_article(path, recommend_permalinks):
@@ -56,13 +67,8 @@ def main():
     path_dir = os.path.join(os.path.dirname(__file__), '..', '_content', 'note')
     paths = [os.path.join(path_dir, f) for f in os.listdir(path_dir)]
 
-    similarities = compute_similarities(paths)
-
-    permalink_extractor = re.compile('(/note/.+?)(\.md|\.html)')
-    for i, path in enumerate(paths):
-        # find top-3 most-similar articles (except for target article itself which is similarity=1.0)
-        top_indices = np.argsort(similarities[i, :])[::-1][1:4]
-        process_article(path, [permalink_extractor.search(paths[j]).group(1) + '/' for j in top_indices])
+    for path, recos in recommend_content_based_cf(paths, k=3):
+        process_article(path, recos)
 
 
 if __name__ == '__main__':
